ebf11d23c4c566ac2ff9d47d8be6dbaccced39bf
Nico Rithner <nicorithner@gmail.com>
2021-10-25 08:54:08 -0600

    Complete lesson 32. Create a POST request

--- second-app/app.js
+++ second-app/app.js
@@ -1,8 +1,16 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  // process.exit(); // unless prompted to exit the server keeps running.
+  const url = req.url;
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Messsage</title></head>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write('</html>');
+    return res.end();
+  }
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>My First Page</title></head>');
