defd0f9897407eb25bf89f44b00c5f449583b1a5
Nico Rithner <nicorithner@gmail.com>
2021-11-01 15:24:15 -0600

    Complete lesson 63. Handling different routes

--- node-express/app.js
+++ node-express/app.js
@@ -2,13 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('in the middleware');
-  next();
+app.use('/add-product', (req, res, next) => {
+  res.send('<h1>Add Product Page</h1>');
 });
 
-app.use((req, res, next) => {
-  console.log('another middleware');
+app.use('/', (req, res, next) => {
   res.send('<h1>Hello</h1>');
 });
 
